fe7700f908ae8a49e18726984c4b1971cf24a5d4
Ayush <ayush.yadav.100100@gmail.com>
2023-12-04 04:35:06 +0530

    initial changes

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,36 +1,50 @@
-import React from "react";
+import React from 'react'
 import {
   Dashboard,
   LibraryBooks,
   PointOfSale,
   School,
   VideoCall,
-} from "@mui/icons-material/";
+} from '@mui/icons-material/'
+import styles from './sidebar.module.scss'
+import Link from 'next/link'
+import { usePathname, useRouter } from 'next/navigation'
 
 const sidebarOptions = [
-  { title: "Dashboard", icon: <Dashboard /> },
-  { title: "Classes", icon: <LibraryBooks /> },
-  { title: "Meetings", icon: <VideoCall /> },
-  { title: "Students", icon: <School /> },
-  { title: "Accounts", icon: <PointOfSale /> },
-];
+  { title: 'Dashboard', icon: <Dashboard />, link: '/a/dashboard' },
+  { title: 'Classes', icon: <LibraryBooks />, link: '/a/classes' },
+  { title: 'Meetings', icon: <VideoCall />, link: '/a/meetings' },
+  { title: 'Students', icon: <School />, link: '/a/students' },
+  { title: 'Accounts', icon: <PointOfSale />, link: '/a/accounts' },
+]
 
 function Sidebar() {
+  const pathname = usePathname().split('/')
   return (
-    <div>
-      <div></div>
-      <div>
+    <div className={styles.sidebarContainer}>
+      <div className={styles.dp}></div>
+      <div className={styles.sidebarOptions}>
         {sidebarOptions.map((option, i) => {
           return (
-            <div key={i}>
-              {option.icon}
-              {option.title}
-            </div>
-          );
+            <Link href={option.link} key={i}>
+              <div
+                className={styles.options}
+                style={{
+                  color:
+                    option.title.toLowerCase() === pathname[2]
+                      ? '#fff'
+                      : 'lightgray',
+                }}
+              >
+                <span>{option.icon}</span>
+                <p>{option.title}</p>
+              </div>
+            </Link>
+          )
         })}
       </div>
     </div>
-  );
+  )
 }
 
-export default Sidebar;
+export default Sidebar

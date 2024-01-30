f4aaf18114b9974079728dc3b45c5662f4ec4703
Ayush yadav <ayush.yadav.100100@gmail.com>
2024-01-30 13:14:06 +0000

    services update

---
+++ src/services/ApiConfig.jsx
@@ -0,0 +1,12 @@
+const apiUrl = 'https://brainstarapi.azurewebsites.net';
+
+export const apiConfig = {
+    //auth
+    register:`${apiUrl}/Auth/register`,
+    login:`${apiUrl}`,
+    refreshToken :`${apiUrl}/Auth/login`,
+
+    //students
+
+
+}

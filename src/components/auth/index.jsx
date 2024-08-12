2e9bf2e86258b9b211075c872ac40d3f05271168
Ayush <ayush.yadav.100100@gmail.com>
2024-08-12 18:23:38 +0530

    update

--- src/components/auth/index.jsx
+++ src/components/auth/index.jsx
@@ -1,21 +1,25 @@
-import { Dialog } from '@mui/material'
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React from "react";
+import { Dialog } from "@mui/material";
+import { useSelector } from "react-redux";
+import styles from "./auth.module.scss";
 
 export default function AuthPopup() {
   const { authPopup } = useSelector(({ user }) => ({
     authPopup: user.authPopup,
-  }))
+  }));
 
-  console.log('authPopup', authPopup)
+  console.log("authPopup", authPopup);
 
-  const handleClose = () => {}
+  const handleClose = () => {};
 
   return (
     <div>
       <Dialog onClose={handleClose} open={authPopup}>
-        hi
+        <div className={styles.authWrapper}>
+          <div className={styles.imgWrapper}></div>
+          <div className={styles.formWrapper}></div>
+        </div>
       </Dialog>
     </div>
-  )
+  );
 }
